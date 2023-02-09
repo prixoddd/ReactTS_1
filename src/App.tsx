6f2ec89b7d29e6d6df9e105def61c4501409b72e
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-02-09 22:02:09 +0200

    09.02 1

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
             <Rating value={2} />
             <Rating value={3} />
             <Rating value={4} />
-            <Rating value={5} />
+            <Rating value={5} /> 
         </div>
     );
 }
