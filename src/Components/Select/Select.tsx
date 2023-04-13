46efd9a8d73dee6814e859c12f29ddaec96166a0
Nikita MK <nikita.derjugin@gmail.com>
2023-04-13 14:42:35 +0300

    majaka 1

--- src/Components/Select/Select.tsx
+++ src/Components/Select/Select.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import {string} from 'prop-types';
 import s from './Select.module.css'
+import {Simulate} from "react-dom/test-utils";
+import keyUp = Simulate.keyUp;
 
 type ItemType = {
     title: string
@@ -31,15 +33,39 @@ export function Selectin(props: SelectPropsType) {
         // setIsOpen(!isOpen)
     }
 
+    const [selected, setSelected] = useState(0)
+    const onMouseEnterHandler = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+        setSelected(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if(e.key === "ArrowUp") {
+            setSelected(selected - 1)
+        } else if(e.key === "ArrowDown") {
+            setSelected(selected + 1)
+        }
+        // console.log(e.key)
+    }
+
     return <div className={s.general}>
+
         {/*{nameTitle}*/}
 
 
-    <div className={s.maindiv} onClick={onTitleClickHandler}>
-        {props.items.map(el => el.value === value && el.title)}
-        <ul>
-            {isOpen && props.items.map((el,index) => el.value !== value && <li onClick={onTitleElementClickHandler} value={el.value} key={index}>{el.title}</li>)}
-        </ul>
-    </div>
+        <div className={s.maindiv} onClick={onTitleClickHandler} onKeyDown={onKeyPressHandler} tabIndex={0}>
+            {props.items.map(el => el.value === value && el.title)}
+            <ul>
+                {isOpen && props.items.map((el, index) => el.value !== value &&
+                    <li className={el.value === selected ? s.selected : ''}
+                        onClick={onTitleElementClickHandler}
+                        onMouseEnter={onMouseEnterHandler}
+                        onMouseLeave={()=>{setSelected(0)}}
+
+                        value={el.value}
+                        key={index}
+                    >
+                        {el.title}</li>)}
+            </ul>
+        </div>
     </div>
 }
